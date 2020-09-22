00b8a84bdcd9c7e75c2c6e8e112cc7b85f85f96d
suposk <jan.supolik@gmail.com>
2020-09-22 17:51:48 +0200

    sampeauth provider not complete

--- V2.0/AppModelv2-WebApp-OpenIDConnect-DotNet-master/AppModelv2-WebApp-OpenIDConnect-DotNet/Controllers/ClaimsController.cs
+++ V2.0/AppModelv2-WebApp-OpenIDConnect-DotNet-master/AppModelv2-WebApp-OpenIDConnect-DotNet/Controllers/ClaimsController.cs
@@ -1,4 +1,15 @@
-﻿using System.Web.Mvc;
+﻿//using AppModelv2_WebApp_OpenIDConnect_DotNet.Models;
+using AppModelv2_WebApp_OpenIDConnect_DotNet.Providers;
+//using Microsoft.Graph;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+//using SecureScore = AppModelv2_WebApp_OpenIDConnect_DotNet.Models.SecureScore;
 
 namespace AppModelv2_WebApp_OpenIDConnect_DotNet.Controllers
 {
@@ -10,6 +21,13 @@ namespace AppModelv2_WebApp_OpenIDConnect_DotNet.Controllers
         /// </summary>
         /// <returns></returns>
         public ActionResult Index()
+        {
+            ReadValues();
+
+            return View();
+        }
+
+        private void ReadValues()
         {
             var userClaims = User.Identity as System.Security.Claims.ClaimsIdentity;
 
@@ -24,8 +42,160 @@ namespace AppModelv2_WebApp_OpenIDConnect_DotNet.Controllers
 
             // TenantId is the unique Tenant Id - which represents an organization in Azure AD
             ViewBag.TenantId = userClaims?.FindFirst("http://schemas.microsoft.com/identity/claims/tenantid")?.Value;
+        }
 
-            return View();
+        //public async Task Token()
+        public async Task<ActionResult> Token()
+        {
+            ReadValues();
+
+            string accessToken = await SampleAuthProvider.Instance.GetUserAccessTokenAsync();
+            ViewBag.AccessToken = accessToken;
+            //string accessToken = SampleAuthProvider.Instance.AccessToken;
+
+            var me = await this.GetMe(accessToken);
+            var score = await this.GetScore(accessToken);
+            var alers = await this.GetAlerts(accessToken, "?$top=1");
+            //var secureScores = await this.GetSecureScore(accessToken, "");
+
+            //var authenticationProvider = new DelegateAuthenticationProvider(
+            //(requestMessage) =>
+            //{
+            //    requestMessage.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            //    return Task.FromResult(0);
+            //});
+            //var graphClient = new GraphServiceClient(authenticationProvider);
+
+            //try
+            //{
+
+            //    var alerts = await graphClient.Security.Alerts
+            //        .Request()
+            //        .GetAsync();
+
+            //    var scores = await graphClient.Security.SecureScores
+            //        .Request()
+            //        .Top(1)
+            //        .GetAsync();
+            //}
+            //catch (Exception ex)
+            //{
+            //    Debug.WriteLine($"Error: {ex.Message} {ex?.InnerException}");
+            //}
+            return View("Index");
+            //return View();
+        }
+
+
+        //public async Task<List<SecureScore>> GetAlerts(string accessToken, string queryParameter)
+        public async Task<List<object>> GetAlerts(string accessToken, string queryParameter)
+        {
+            try
+            {
+                string endpoint = "https://graph.microsoft.com/v1.0/security/alerts"; queryParameter = string.Empty;
+
+                using (var client = new HttpClient())
+                {
+                    using (var request = new HttpRequestMessage(HttpMethod.Get, endpoint + queryParameter))
+                    {
+                        request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+                        using (var response = await client.SendAsync(request))
+                        {
+                            if (response.IsSuccessStatusCode)
+                            {
+                                string result = await response.Content.ReadAsStringAsync();
+                                //SecureScoreResult secureScoreResult = JsonConvert.DeserializeObject<SecureScoreResult>(result);
+                                //return secureScoreResult.Value;
+                                return null;
+                            }
+                            else
+                            {
+                                Debug.WriteLine($"Error: {response}");
+                                return null;
+                            }
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public async Task<string> GetMe(string accessToken, string endpoint = null)
+        {
+            try
+            {
+                string url = endpoint ?? "https://graph.microsoft.com/v1.0/me/";
+
+                using (var client = new HttpClient())
+                {
+                    using (var request = new HttpRequestMessage(HttpMethod.Get, url))
+                    {
+                        request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+                        using (var response = await client.SendAsync(request))
+                        {
+                            if (response.IsSuccessStatusCode)
+                            {
+                                string result = await response.Content.ReadAsStringAsync();
+                                return result;
+                            }
+                            else
+                            {
+                                Debug.WriteLine($"Error: {response}");
+                                return null;
+                            }
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        //public async Task<List<SecureScore>> GetScore(string accessToken, string endpoint = null)
+        public async Task<List<object>> GetScore(string accessToken, string endpoint = null)
+        {
+            try
+            {
+                string url = endpoint ?? "https://graph.microsoft.com/beta/security/secureScores?$top=5";
+
+                using (var client = new HttpClient())
+                {
+                    using (var request = new HttpRequestMessage(HttpMethod.Get, url))
+                    {
+                        request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+                        using (var response = await client.SendAsync(request))
+                        {
+                            if (response.IsSuccessStatusCode)
+                            {
+                                string result = await response.Content.ReadAsStringAsync();
+                                //SecureScoreResult secureScoreResult = JsonConvert.DeserializeObject<SecureScoreResult>(result);
+                                //return secureScoreResult.Value;
+                                return null;
+                            }
+                            else
+                            {
+                                Debug.WriteLine($"Error: {response}");
+                                return null;
+                            }
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                return null;
+            }
         }
     }
 }
---
+++ V2.0/AppModelv2-WebApp-OpenIDConnect-DotNet-master/AppModelv2-WebApp-OpenIDConnect-DotNet/Providers/SampleAuthProvider.cs
@@ -0,0 +1,102 @@
+﻿using AppModelv2_WebApp_OpenIDConnect_DotNet.TokenStorage;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.Identity.Client;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.OpenIdConnect;
+
+namespace AppModelv2_WebApp_OpenIDConnect_DotNet.Providers
+{
+    public sealed class SampleAuthProvider
+    {
+        // The Client ID is used by the application to uniquely identify itself to Azure AD.
+        string clientId = ConfigurationManager.AppSettings["ClientId"];
+        // RedirectUri is the URL where the user will be redirected to after they sign in.
+        string redirectUri = ConfigurationManager.AppSettings["RedirectUri"];
+        // Tenant is the tenant ID (e.g. contoso.onmicrosoft.com, or 'common' for multi-tenant)
+        string tenant = ConfigurationManager.AppSettings["Tenant"];
+        string graphScopes = System.Configuration.ConfigurationManager.AppSettings["GraphScopes"].ToLower();
+        string appSecret = ConfigurationManager.AppSettings["ida:AppSecret"];
+
+
+        private static readonly SampleAuthProvider SampleInstance = new SampleAuthProvider();
+        private SampleAuthProvider()
+        {
+        }
+
+        private SessionTokenCache TokenCache { get; set; }
+        public string AccessToken { get; set; }
+
+        public static SampleAuthProvider Instance
+        {
+            get
+            {
+                return SampleInstance;
+            }
+        }
+
+        // Gets an access token. First tries to get the token from the token cache.
+        public async Task<string> GetUserAccessTokenAsync()
+        {
+            string signedInUserID = ClaimsPrincipal.Current.FindFirst(ClaimTypes.NameIdentifier).Value;
+            HttpContextWrapper httpContext = new HttpContextWrapper(HttpContext.Current);
+            TokenCache userTokenCache = new SessionTokenCache(signedInUserID, httpContext).GetMsalCacheInstance();
+            //// var cachedItems = tokenCache.ReadItems(appId); // see what's in the cache
+            ///
+
+            ConfidentialClientApplication cca = null;
+
+            //cca = new ConfidentialClientApplication(
+            //    this.clientId,
+            //    this.redirectUri,
+            //    new ClientCredential(this.appSecret),
+            //    userTokenCache,
+            //    null);
+
+            try
+            {
+                //string[] scopes = graphScopes.Split(new char[] { ' ' });
+                //var scopes = new List<string> { "user.read" };
+                var scopes = new List<string> { AppConstants.SCOPE };
+
+                //string[] ascopes = graphScopes.Split(new char[] { ' ' });
+                //var scopes = new List<string> { AppConstants.SCOPE };
+                //scopes.AddRange(ascopes);
+
+                IEnumerable<IAccount> accounts = await cca.GetAccountsAsync();
+                IAccount firstAccount = accounts.FirstOrDefault();                
+                if (firstAccount != null)
+                {
+                    var result = await cca.AcquireTokenSilent(graphScopes.Split(new char[] { ' ' }), firstAccount).ExecuteAsync();
+                    return result.AccessToken;
+                }
+                else
+                {
+                    var result = await cca.AcquireTokenForClient(scopes).ExecuteAsync();
+                    return result.AccessToken;
+                }                
+            }
+            catch (Exception ex)
+            {
+                //// Unable to retrieve the access token silently.
+                HttpContext.Current.Request.GetOwinContext().Authentication.Challenge(
+                    new AuthenticationProperties() { RedirectUri = "/" },
+                    OpenIdConnectAuthenticationDefaults.AuthenticationType);
+
+                //throw new ServiceException(
+                //    new Error
+                //    {
+                //        Code = GraphErrorCode.AuthenticationFailure.ToString(),
+                //        Message = " Errror in Auth JANO",
+                //    });
+
+                throw;
+            }
+        }
+    }
+}
